5caf6ff2ad5bd3c09f2e1b7bd690c46d2ac5b366
futamidaiki <nimi0370@gmail.com>
2018-03-03 00:31:05 +0900

    Added state and FastList

--- App.js
+++ App.js
@@ -10,26 +10,57 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FastList,
 } from 'react-native';
 // TodoInputモジュールを読み込む
 import TodoInput from './src/component/TodoInput';
+import TodoItem from './src/component/TodoItem';
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+    };
+  }
     onPress = (text) => {
-    console.log(text);
+    const list = [].concat(this.state.list);
+
+    list.push({
+      key: Date.now(),
+      text: text,
+      done: false,
+    });
+
+    this.setState({
+      list,
+    });
   }
 
   render() {
+    const {
+      list,
+    } = this.state;
+
     return (
       <View style={styles.container}>
         <View style={styles.main}>
           <TodoInput onPress={this.onPress} />
+          <View style={styles.todoListContainer}>
+            <FlatList
+              style={styles.todoList}
+              data={list}
+              renderItem={({ item }) => <TodoItem {...item} />}
+            />
+          </View>
         </View>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -39,8 +70,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   main: {
-  flex: 1,
-  maxWidth: 400,
-  alignItems: 'center',
-}
+    flex: 1,
+    maxWidth: 400,
+    alignItems: 'center',
+  },
+  todoListContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  todoList: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
 });
